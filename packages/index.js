667ed9667ffe4878aac963427704fa515da2f9ad
yongzhi.liu <yongzhi1.liu@gmail.com>
2022-04-16 21:46:22 +0800

    兼容cdn

--- packages/index.js
+++ packages/index.js
@@ -4,10 +4,15 @@ const components = [
   OpenInDefaultBrowser
 ]
 
+const install = (Vue) => {
+  components.forEach(component => {
+    Vue.component(component.name, component)
+  })
+}
+// 兼容cdn环境
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
 export default {
-  install: (Vue) => {
-    components.forEach(component => {
-      Vue.component(component.name, component)
-    })
-  }
+  install
 }
